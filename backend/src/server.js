172ff429b31eff844b4b55bb461b75a95ecce975
Ahmed527 <naveedahmed151106@gmail.com>
2025-10-10 20:11:08 +0530

    message routes implemented

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,6 +4,7 @@ import path from 'path';
 import cookieParser from 'cookie-parser';
 
 import authRoutes from './routes/auth.route.js';
+import messageRoutes from './routes/message.route.js';
 import { connectDB } from './lib/db.js';
 
 
@@ -17,6 +18,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use('/api/auth', authRoutes);
+app.use("/api/messages", messageRoutes);
 
 if(ENV.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../frontend/dist')));
