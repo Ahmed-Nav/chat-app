de136ac7158a2b2b33c1fbe6fa496a1a33a10d16
Ahmed527 <naveedahmed151106@gmail.com>
2025-10-11 12:42:36 +0530

    sidebar updated

--- frontend/src/store/useAuthstore.js
+++ frontend/src/store/useAuthstore.js
@@ -56,5 +56,15 @@ export const useAuthStore = create((set) => ({
     } catch (error) {
       console.log("Error Logging Out", error);
     }
-  }
+  },
+
+  updateProfile: async (data) => {
+    try {
+      const res = await axiosInstance.put("/auth/update-profile", data);
+      set({ authUser: res.data });
+      toast.success("Profile updated successfully");
+    } catch (error) {
+      toast.error(error.response.data.message);
+    }
+  },
 }));
